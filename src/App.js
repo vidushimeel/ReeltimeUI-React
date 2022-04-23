cb08fb9cfe531f9fe14aa6945f6f0bccb8bc5e52
Vidushi <meelvidushi1@gmail.com>
2022-04-23 14:34:42 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="sidePanel">
+        <h1 id="login-header" class = "center">Welcome to Reeltime!</h1>
+      <div class="username-box">
+      <p class = "username-leftalign">Enter any username...</p>
+      </div>
+    </div>
+  )
+}
+
+export default App;
