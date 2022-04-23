31f0ef6aa8f0e66040fd4705471c42e886710f5d
Vidushi <meelvidushi1@gmail.com>
2022-04-23 14:56:22 -0400

    Div formatting

--- src/App.js
+++ src/App.js
@@ -4,8 +4,12 @@ function App() {
   return (
     <div class="sidePanel">
         <h1 id="login-header" class = "center">Welcome to Reeltime!</h1>
-      <div class="username-box">
+      <div class="username-div">
       <p class = "username-leftalign">Enter any username...</p>
+      </div>
+      <div class = "terms-div ">
+      <p> By registering, you agree to Reeltime’s <a href="google.com">Terms of Service</a>  and <a href="google.com">Privacy Policy</a> </p>
+
       </div>
     </div>
   )
